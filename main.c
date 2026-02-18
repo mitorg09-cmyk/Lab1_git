bca86319ed1a8f770baf05881332089a15c7b5c3
Mitrofan <mityirg09@mail.ru>
2026-02-18 13:58:20 +0700

    Test Commit

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#define size 256
+#define wLen 4
+#define separSize 7
 
 int main()
 {
-    printf("Hello World!\n");
+    char arr[size] = "abcd Abcd abcd, ughabcd - hababcd: abcd-abcd-h";
+    char buff[wLen] = "abcd";
+    char separators[separSize] = {' ', ',', ':', '-', '(', ')', '.'};
+    int begEnd[size] = {0};
+
+    int arrlen = strlen(arr);
+    int j = 0;
+    for(int i = 0; i < arrlen && arr[i] != '\0'; i++)
+    {
+        int k = 0;
+        for(; k < separSize && arr[i] != separators[k]; k++)
+        {
+        }
+        if(k == separSize)
+        {
+            j++;
+        }
+        else
+        {
+            j = 0;
+        }
+        if(j == wLen)
+        {
+
+        }
+    }
+
     return 0;
 }
