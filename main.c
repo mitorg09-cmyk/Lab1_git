01e3f9bf028f1977475ea968d78d954c4f13f41b
Mitrofan <mityirg09@mail.ru>
2026-02-23 11:46:16 -0500

    Added tests
    Добавлены 13 тестовых массивов с исчерпыващими тестировочными
    данными. Тестирование производится путем изменения индекса
    в названии массива в директиве define.

--- main.c
+++ main.c
@@ -3,10 +3,24 @@
 #define size 256
 #define max 2147483647
 #define min -2147483648
+#define arr arr0
 
 int main()
 {
-    char arr[size] = "001";
+    char arr0[size] = "0"; // 0
+    char arr1[size] = "1"; // 1
+    char arr2[size] = "-1"; // -1
+    char arr3[size] = "-0"; // 0
+    char arr4[size] = "+1"; // 1
+    char arr5[size] = "2147483647"; // max
+    char arr6[size] = "-2147483648"; // min
+    char arr7[size] = "2147483648"; // max + 1 >> Overflow
+    char arr8[size] = "-2147483649"; // min - 1 >> Overflow
+    char arr9[size] = "11qqq"; // Invalid symbol
+    char arr10[size] = "-aaa"; // Invalid symbol
+    char arr11[size] = "-99999999999999999999999"; // Overflow
+    char arr12[size] = "+99999999999999999999999"; // Overflow
+
     int result = 0;
     int isNegat = 0;
 
