c057eefc829dc2d22b43b4714a9a4192ee0ba2e5
Mitrofan <mityirg09@mail.ru>
2026-02-23 07:05:53 -0500

    Added base functional
    Добалена основная функциональность программы.
    Добавлен основной цикл перебора символов в сходной строке
    и необходимая логика.
    Добавлен массив разделителей и массив для искомого слова.
    
    ЗАМЕТКА: необходимо обработать случай с искомым словом в конце
    исходной строки.
    
    Логика работы программы заключается в линейном поиске
    комбинаций символом между разделителями, длина которых
    совпадает с искомым словом. При совпадении комбинация
    сравнивается с искомым словом напрямую посимвольно.
    
    Несмотря на два вложенный цикла, от исходных данных зависит
    только внешний, внутрение выполняют фиксированное число
    итераций.

--- main.c
+++ main.c
@@ -6,17 +6,19 @@
 
 int main()
 {
-    char arr[size] = "abcd Abcd abcd, ughabcd - hababcd: abcd-abcd-h";
+    char arr[size] = "abcd abcd abcd, ughabcd - hababcd: abcd-abcd-h abcd ";
+
     char buff[wLen] = "abcd";
     char separators[separSize] = {' ', ',', ':', '-', '(', ')', '.'};
-    int begEnd[size] = {0};
+    //int begEnd[size] = {0};
+    int count = 0;
 
     int arrlen = strlen(arr);
     int j = 0;
-    for(int i = 0; i < arrlen && arr[i] != '\0'; i++)
+    for(int i = 0; i < arrlen || arr[i] != '\0'; i++)
     {
         int k = 0;
-        for(; k < separSize && arr[i] != separators[k]; k++)
+        for(; k < separSize && arr[i] != separators[k] && arr[i] != '\0'; k++)
         {
         }
         if(k == separSize)
@@ -25,13 +27,23 @@ int main()
         }
         else
         {
-            j = 0;
-        }
-        if(j == wLen)
-        {
-
+          if(j == wLen)
+          {
+            int cntr = i - wLen;
+            int m = 0;
+            for(; m < wLen && arr[cntr] == buff[m]; m++, cntr++)
+            {
+            }
+            if(m == wLen)
+            {
+              count++;
+            }
+          }
+          j = 0;
         }
     }
+
+    printf("count of words is: %d \n", count);
 
     return 0;
 }
