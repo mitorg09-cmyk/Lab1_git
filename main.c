ac139055a4889ea1c67e86c0e02f082aa8b5deae
Mitrofan <mityirg09@mail.ru>
2026-03-04 00:54:14 -0500

    Added functions cmpStrAlphabetOrder
     Функция cmpStrAlphabetOrder принимает на вход два указателя типа char, то есть две строки. Сделано это в угоду инкапсуляции, проверки в функции выполнены.
    Возвращает 1, если первое слово в параметрах стоит первее в алфавитном порядке и 0, если второе.

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
+int cmpStrAlphabetOrder(char* str1, char* str2);
+
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+  char str0[10] = "abcd,";
+  char str1[10] = "aacd,";
+  char str2[10] = "aa,";
+  char str3[10] = "a,";
+
+  printf("%d \n", cmpStrAlphabetOrder(str3, str2));
+}
+
+int cmpStrAlphabetOrder(char* str1, char* str2)
+{
+  for(int i = 0; ; i++)
+  {
+    if(str1[i] == ',' || str1[i] == '.') return 1;
+    if(str2[i] == ',' || str2[i] == '.') return 0;
+
+    if(str1[i] < str2[i])
+    {
+      return 1;
+    }
+
+    if(str1[i] > str2[i])
+    {
+      return 0;
+    }
+  }
 }
