38a45f7d6143f80f9be4b284e976d89cbb6a32f9
Mitrofan <mityirg09@mail.ru>
2026-03-04 00:56:40 -0500

    Add logic for '\0' symbol
    Добавлена обработка ситуации с отсутствием разделителя в конце строки, обработка терминального символа.

--- main.c
+++ main.c
@@ -7,26 +7,30 @@ int main()
   char str0[10] = "abcd,";
   char str1[10] = "aacd,";
   char str2[10] = "aa,";
-  char str3[10] = "a,";
+  char str3[10] = "a";
 
   printf("%d \n", cmpStrAlphabetOrder(str3, str2));
 }
 
 int cmpStrAlphabetOrder(char* str1, char* str2)
 {
-  for(int i = 0; ; i++)
+  if(str1 && str2)
   {
-    if(str1[i] == ',' || str1[i] == '.') return 1;
-    if(str2[i] == ',' || str2[i] == '.') return 0;
-
-    if(str1[i] < str2[i])
+    for(int i = 0; ; i++)
     {
-      return 1;
-    }
+      if(str1[i] == ',' || str1[i] == '.' || str1[i] == '\0') return 1;
+      if(str2[i] == ',' || str2[i] == '.' || str1[i] == '\0') return 0;
 
-    if(str1[i] > str2[i])
-    {
-      return 0;
+      if(str1[i] < str2[i])
+      {
+        return 1;
+      }
+
+      if(str1[i] > str2[i])
+      {
+        return 0;
+      }
     }
   }
+  else return 2;
 }
