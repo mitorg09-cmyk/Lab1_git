3727a620f8ed16bcd7fff7920b6db9d3a6a328b2
Mitrofan <mityirg09@mail.ru>
2026-03-04 01:56:35 -0500

    Added ValidateStrSqueez function
    Функция ValidateStrSqueez валидирует строку, переданную в параметрах. Вывод: 0 - строка валидирована и сжата (под сжатием подразумевается удаление подряд идущих разделителей с заменой их на запятые); 1 - валидация не удалась или строка не валидна. Создает внутренния Tab для валидации.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int cmpStrAlphabetOrder(char* str1, char* str2);
+int ValidateStr(char* str1);
 
 int main()
 {
@@ -8,6 +9,8 @@ int main()
   char str1[10] = "aacd,";
   char str2[10] = "aa,";
   char str3[10] = "a";
+  char o = 234;
+  printf("%c \n", o);
 
   printf("%d \n", cmpStrAlphabetOrder(str3, str2));
 }
@@ -33,4 +36,31 @@ int cmpStrAlphabetOrder(char* str1, char* str2)
     }
   }
   else return 2;
+}
+
+int ValidateStr(char* str1) // 0 - validation success; 1 - validation failure or invalid str.
+{
+  if(str1)
+  {
+    int validationTab[256] = {0}; // 0 - Valid symbol; 1 - Valid symbol-separator; 2 - Invalid symbol.
+
+    for(int i = 65; i <= 90; i++){validationTab[i] = 0;} // A - Z.
+    for(int i = 97; i <= 122; i++){validationTab[i] = 0;} // a - z.
+
+    for(int i = 1; i <= 31; i++){validationTab[i] = 2;}
+    for(int i = 33; i <= 43; i++){validationTab[i] = 2;}
+    for(int i = 45; i <= 31; i++){validationTab[i] = 2;}
+    for(int i = 127; i <= 255; i++){validationTab[i] = 2;}
+    validationTab[45] = 2;
+
+    validationTab[32] = 1; // ' '
+    validationTab[44] = 1; // ','
+    validationTab[46] = 1; // '.'
+
+
+  }
+  else
+  {
+    return 1;
+  }
 }
