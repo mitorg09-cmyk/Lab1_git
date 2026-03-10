19e243303d97cb9782a4bc3f4fefeef1c46116e2
Mitrofan <mityirg09@mail.ru>
2026-03-10 16:32:23 -0400

    ValidateStrSqueezz -> ValidateFindeStr
    Функция ValidateStrSqueezz была переделана в ValidateFindeStr. Теперь исходные данный, передаваемые в функцию по адресу, не изменяются. Функция находит индексы слов
    и их длины и записывает их в выходной массив, переданный в аргументы. В функцию ValidateFindeStr теперь передается пара новых аргументов помимо выходного массива -
    длины строки и выходного массива, что повышает безопасность кода.
    Была переделана сигнатура функции cmpStrAlphabetOrder, теперь она принимает в параметры еще
    два аргумента - длины слов, ведь функция работает не до терминальных символов, как было ранее (требует доработки).

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int cmpStrAlphabetOrder(char* str1, char* str2);
-int ValidateStrSqueez(char* str1);
+int cmpStrAlphabetOrder(char* str1, char* str2, size_t l1, size_t l2);
+int ValidateFindStr(char* str1, size_t strLen, int* outMas, size_t outMasLen);
 
 int main()
 {
-  char str0[10] = "abcd,";
-  char str1[10] = "aacd,";
-  char str2[10] = "aa,";
-  char str3[10] = "a";
-  char o = 234;
-  printf("%c \n", o);
+  char str0[128] = "abcd,abcd,gggggg,udgngf,d,    fg  d fasdf, df ,df ,d ,d. f.d ,df...,abcd,a,ab,aaab,aa,aa,a,abcd,abcda";
+  int indxLnWrd[256] = {0};
 
-  printf("%d \n", cmpStrAlphabetOrder(str3, str2));
+  ValidateFindStr(str0, 128, indxLnWrd, 256);
+
+  for(int i = 0; indxLnWrd[i] != -1; i += 2)
+  {
+    for(int j = i + 2; indxLnWrd[j] != -1; j += 2)
+    {
+      if(ValidateFindStr(str0 + indxLnWrd[i], str0 + indxLnWrd[j], indxLnWrd[i + 1], indxLnWrd[j + 1])){}
+    }
+  }
 }
 
-int cmpStrAlphabetOrder(char* str1, char* str2)
+int cmpStrAlphabetOrder(char* str1, char* str2, size_t l1, size_t l2)
 {
   if(str1 && str2)
   {
@@ -38,9 +43,9 @@ int cmpStrAlphabetOrder(char* str1, char* str2)
   else return 2;
 }
 
-int ValidateStrSqueez(char* str1) // 0 - validation success; 1 - validation failure or invalid str.
+int ValidateFindStr(char* str1, size_t strLen, int* outMas, size_t outMasLen) // 0 - validation success and found words; 1 - validation failure or invalid str.
 {
-  if(str1)
+  if(str1 && outMas && outMasLen >= strLen * 2)
   {
     int validationTab[256] = {0}; // 0 - Invalid symbol; 1 - Valid symbol-separator; 2 - Valid symbol.
 
@@ -52,14 +57,35 @@ int ValidateStrSqueez(char* str1) // 0 - validation success; 1 - validation fail
     validationTab[44] = 1; // ','
     validationTab[46] = 1; // '.'
 
-    for(int i = 0; str1[i] != '\0'; i++)
+    int wordCounter = 0;
+    if(validationTab[str1[0]] == 2)
+    {
+      outMas[wordCounter] = 0;
+      wordCounter += 2;
+      //for(int i = 0; validationTab[str1[i]] == 2; i++) outMas[wordCounter]++;
+    }
+
+    for(int i = 0; i < outMasLen; i++){outMas[i] = 0;}
+
+    size_t len = 0;
+    for(int i = 0; str1[i + 1] != '\0'; i++)
     {
       if(validationTab[str1[i]])
       {
-
+        if(validationTab[str1[i]] == 1 && validationTab[str1[i + 1]] == 2)
+        {
+          outMas[wordCounter] = i + 1;
+          wordCounter += 2;
+        }
+        if(validationTab[str1[i]] == 2)
+        {
+          outMas[wordCounter - 1]++;
+        }
       }
       else return 1;
     }
+    outMas[wordCounter] = -1;
+    outMas[wordCounter + 1] = -1;
   }
   else
   {
