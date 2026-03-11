752bc4faad2616b54d32be642cd0b1d03e41f6f3
Mitrofan <mityirg09@mail.ru>
2026-03-11 02:44:29 -0400

    Added function ValidateFindStr

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #define separSize 7
 #define arr arr0
 
+int ValidateFindStr(char* str1, size_t strLen, int* outMas, size_t outMasLen);
+
 int main()
 {
     // For wLen == 4
@@ -55,4 +57,56 @@ int main()
     printf("count of words is: %d \n", count);
 
     return 0;
+}
+
+int ValidateFindStr(char* str1, size_t strLen, int* outMas, size_t outMasLen) // 0 - validation success and found words; 1 - validation failure or invalid str.
+{
+  if(outMasLen < strLen * 2) return 2; // 2 - outMas is too small.
+  if(str1 && outMas)
+  {
+    int validationTab[256] = {0}; // 0 - Invalid symbol; 1 - Valid symbol-separator; 2 - Valid symbol.
+
+    for(int i = 65; i <= 90; i++){validationTab[i] = 2;} // A - Z.
+    for(int i = 97; i <= 122; i++){validationTab[i] = 2;} // a - z.
+    validationTab[0] = 2;
+
+    validationTab[32] = 1; // ' '
+    validationTab[44] = 1; // ','
+    validationTab[46] = 1; // '.'
+
+    int wordCounter = 0;
+    if(validationTab[str1[0]] == 2)
+    {
+      outMas[wordCounter] = 0;
+      wordCounter += 2;
+      //for(int i = 0; validationTab[str1[i]] == 2; i++) outMas[wordCounter]++;
+    }
+
+    for(int i = 0; i < outMasLen; i++){outMas[i] = 0;}
+
+    size_t len = 0;
+    for(int i = 0; str1[i] != '\0'; i++)
+    {
+      if(validationTab[str1[i]])
+      {
+        if(str1[i + 1] != '\0' && validationTab[str1[i]] == 1 && validationTab[str1[i + 1]] == 2)
+        {
+          outMas[wordCounter] = i + 1;
+          wordCounter += 2;
+        }
+        if(validationTab[str1[i]] == 2)
+        {
+          outMas[wordCounter - 1]++;
+        }
+      }
+      else return 1;
+    }
+    outMas[wordCounter] = -1;
+    outMas[wordCounter + 1] = -1;
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
 }
